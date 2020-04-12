7c79efdcefcc74cfda3b171cc53455a3a591bb43
Luma <lumagoesmontes@gmail.com>
2020-04-12 17:36:57 -0300

    Configurando Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,36 @@ import { View, Image,Text, StyleSheet} from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
 function Main() {
-   
+    const[devs, setDevs] = useState ({});
+
+    async function loadDevs() {
+        const response = await
+        api.get ('https://www.proesc.com/api/v1/list_students_filter?token=a8d062dc80593f2e2ff421f542a4eadb&unidades=1&ano_letivo=2022')
+    
+        setDevs(response.data.students)    
+    }
+    useEffect (()=>{
+        loadDevs()
+    },[])
+    return (
+        <View>
+            <FlatList
+                data={devs}
+                keyExtractor={dev => String(dev.id)}
+                renderItem={({item: dev}) => (
+            <View style={styles.container}>
+                <Image style={styles.avatar} source={{ uri: dev.photo }}></Image>
+                <Text style={styles.name}>{dev.name}</Text>
+                <Text style={styles.class}>{dev.class}</Text>
+            </View>
+            )}
+            >
+                </FlatList>
+        </View>
+    )
 }
+
+
+    
+
 export default Main;
